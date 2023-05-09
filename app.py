8cbc024ab8dc5d32794dc17b23e91e4ca381f031
Your Name <you@example.com>
2023-05-10 02:36:42 +0300

    updated faker and flask modules

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
 from faker import Faker
 
-fake = Faker()
 
 @app.route('/')
-def index():
-    names = []
+def get_a_name():
+    name = []
+    fake = Faker()
+    letter = request.args.get('letter', default='A', type=str)
     for _ in range(10):
-        names.append(fake.name())
-    return render_template('project.html', names=names)
+        name = fake.first_name()
+    return render_template('project.html', letter=letter, name=name)
 
 if __name__ == '__main__':
     app.run()
