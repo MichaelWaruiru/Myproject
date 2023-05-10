696bdb571b51b0ca0abe04f18d44304815bf660e
Your Name <you@example.com>
2023-05-10 03:02:55 +0300

    Updated the flask and faker modules

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
-
+from flask import Flask, render_template
 from faker import Faker
+
+# Create the Flask app
+app = Flask(__name__)
 
 
 @app.route('/')
-def get_a_name():
-    name = []
+def index():
+    """Render the project.html template."""
+    return render_template('project.html')
+
+
+@app.route('/names/<letter>')
+def get_a_name(letter):
+    """Generate a dummy name that starts with the given letter."""
+    # Create a Faker instance
     fake = Faker()
-    letter = request.args.get('letter', default='A', type=str)
-    for _ in range(10):
-        name = fake.first_name()
-    return render_template('project.html', letter=letter, name=name)
+    while True:
+        # Generate a female name if the letter is a vowel, otherwise a male name
+        if letter.lower() in ['a', 'e', 'i', 'o', 'u']:
+            name = fake.first_name_female()
+        else:
+            name = fake.first_name_male()
+        # If the name starts with the requested letter, return it
+        if name.startswith(letter.upper()):
+            return name
 
 if __name__ == '__main__':
+    # Run the app
     app.run()
